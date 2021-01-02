b3fdfb40fcfa654fce4fd33bffc79070be74f2d2
Baig, Feroz <feroz.baig96@gmail.com>
2021-01-02 16:39:53 +0530

    offloaded code query params on API-GW

--- lambdas/GenerateTokens.py
+++ lambdas/GenerateTokens.py
@@ -3,22 +3,14 @@ import urllib3
 
 def lambda_handler(event, context):
     
-    try:
-        global code
-        code = event['queryStringParameters']['code']
-    except Exception as e:
-        return {
-            'statusCode': 401,
-            'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,GET'
-            },
-        }
+    code = event['queryStringParameters']['code']
     
+    # TODO change
     TOKEN_ENDPOINT = 'https://auth-userpool.swatcat.ml/oauth2/token'
     CLIENT_ID = 'r6idl4n64lotv1ijlvpv5sb0f'
-    REDIRECT_URI = 'https://lk7kafdzwf.execute-api.us-east-1.amazonaws.com/dev/token'
+    # TODO CloudFront
+    # REDIRECT_URI = 'https://lk7kafdzwf.execute-api.us-east-1.amazonaws.com/dev/token'
+    REDIRECT_URI = 'https://example.com/'
     
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     payload = f'grant_type=authorization_code&client_id={CLIENT_ID}&code={code}&redirect_uri={REDIRECT_URI}'
@@ -27,7 +19,6 @@ def lambda_handler(event, context):
     response = http.request('POST', TOKEN_ENDPOINT, headers=headers, body=payload)
     
     return {
-    # TODO redirect
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Headers': '*',
